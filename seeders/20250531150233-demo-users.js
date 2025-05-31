5d9fec24f0302f3ac227f0a59c15a4685819d71a
Rifqi Afandi <rifqitriafandi.20@gmail.com>
2025-05-31 22:07:13 +0700

    feat: add user seeder with hashed password

---
+++ seeders/20250531150233-demo-users.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+        const hashedPasswords = await Promise.all([
+      bcrypt.hash('admin123', 10)
+    ]);
+      await queryInterface.bulkInsert('Users', [
+        {
+        name: 'Admin',
+        username: 'admin',
+        password: hashedPasswords[0],
+        createdAt: new Date(),
+        updatedAt: new Date()
+     }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
