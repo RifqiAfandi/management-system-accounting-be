7216757a60cd74d898ef9be7ec67329cfad6f23a
RifqiAfandi <rifqitriafandi.20@gmail.com>
2025-06-02 22:39:52 +0700

    refactor: standardize import statements in index.js and ensure proper module exports

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,17 @@
-const authController = require('./authController');
+const authController = require("./authController");
+const salesByCategoryController = require("./salesByCategoryController");
+const dailyOperationalCostController = require("./dailyOperationalCostController");
+const paymentMethodBreakdownController = require("./paymentMethodBreakdownController");
+const salesCategoryController = require("./salesCategoryController");
+const operationalCategoryController = require("./operationalCategoryController");
+const paymentMethodController = require("./paymentMethodController");
 
 module.exports = {
-    authController
+  authController,
+  salesByCategoryController,
+  dailyOperationalCostController,
+  paymentMethodBreakdownController,
+  salesCategoryController,
+  operationalCategoryController,
+  paymentMethodController,
 };
