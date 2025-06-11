f332f68faa0b3755050888e3811681f10cd2216b
Rifqi Afandi <146163545+RifqiAfandi@users.noreply.github.com>
2025-06-11 20:24:20 +0700

    feat: remove daily performance target controller, model, routes, and seeders

--- routes/dailyPerformanceTargetRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dailyPerformanceTargetController = require('../controllers/dailyPerformanceTargetController');
-
-// Daily performance target routes
-router.get('/', dailyPerformanceTargetController.getAllDailyPerformanceTargets);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 // Import auth routes
 const authRoutes = require('./authRoute');
 const dailyOperationalCostRoutes = require('./dailyOperationalCostRoute');
-const dailyPerformanceTargetRoutes = require('./dailyPerformanceTargetRoute');
 const operationalCostTypeRoutes = require('./operationalCostTypeRoute');
 const paymentMethodBreakdownRoutes = require('./paymentMethodBreakdownRoute');
 const paymentTypeRoutes = require('./paymentTypeRoute');
@@ -15,7 +14,6 @@ const trafficAndCustomerRoutes = require('./trafficAndCustomerRoute');
 // Use auth routes
 router.use('/auth', authRoutes);
 router.use('/daily-operational-cost', dailyOperationalCostRoutes);
-router.use('/daily-performance-target', dailyPerformanceTargetRoutes);
 router.use('/operational-cost-type', operationalCostTypeRoutes);
 router.use('/payment-method-breakdown', paymentMethodBreakdownRoutes);
 router.use('/payment-type', paymentTypeRoutes);
